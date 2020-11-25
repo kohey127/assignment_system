61e862d017e320c34846d78aac9624ef8da329c2
kohey127 <koooochoki@gmail.com>
2020-11-25 21:23:16 +0000

    アソシエーション定義

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -1,2 +1,4 @@
 class Commit < ApplicationRecord
+  belongs_to :employee
+  belongs_to :project
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,5 @@
 class Employee < ApplicationRecord
+  belongs_to :department
+  belongs_to :grade
+  has_many :commits, dependent: :destroy
 end
